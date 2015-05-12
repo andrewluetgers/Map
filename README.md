7932fd7075f5dccda140bc0f96889624d439f64e
andrew luetgers <andrew.luetgers@gmail.com>
2015-05-12 00:55:42 -0500

    module support, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Map
 a very basic Map data-structure
+
+
+##usage 
+
+```javascript
+var m = map(
+  [{}, [], "", 0, /foo/, new Date()],
+  ["object", "array", "string", "number", "regexp", "date"]
+);
+
+console.log(m, m.values, m.keys);
+console.log("key of 'number'", m.keyOf('number'));
+console.log("index of 0", m.indexOf(0), "value at index 3", m.values[3]);
+
+m.set(map, "map");
+
+console.log(m.get(map))
+
+console.log("set in", m.setIn(["foo", map, Date, 8], "test"));
+console.log(m.getIn(["foo", map, Date, 8]));
+
+m.each(function(k, v) {
+  console.log("key", k, "value", v);
+});
+```
+
+
+[usage demo](http://plnkr.co/edit/nVPEQwbg1GUW1SyRZyf4?p=preview)
+
--- index.js
+++ index.js
@@ -1,90 +1,107 @@
-function Map(keys, values) {return new _Map(keys, values)}
-function _Map(keys, values) {this.clear(keys, values);}
 
-_Map.prototype = {
+(function() {
+	var root = this, previous_map = root.map;
 
-	set: function(key, value) {
-		var i = this.indexOf(key);
-		this._keys[i] = key;
-		this._values[i] = value;
-		return i !== this._keys.length;
-	},
+	function map(keys, values) {return new Map(keys, values);}
+	function Map(keys, values) {this.clear(keys, values);}
 
-	get: function(key) {
-		return this._values[this.indexOf(key)];
-	},
+	Map.prototype = {
 
-	remove: function(key) {
-		var i = this.indexOf(key);
-		if (i !== this._keys.length) {
-			this._keys.splice(i, 1);
-			this._values.splice(i, 1);
-		}
-		return this;
-	},
+		set: function(key, value) {
+			var i = this.indexOf(key);
+			this.keys[i] = key;
+			this.values[i] = value;
+			return i !== this.keys.length;
+		},
 
-	clear: function(keys, values) {
-		this._keys = keys || [];
-		this._values = values || [];
-		return this;
-	},
+		get: function(key) {
+			return this.values[this.indexOf(key)];
+		},
 
-	keys: function() {
-		return [].concat(this._keys);
-	},
+		getIn: function(keyPath) {
+			var current = this,
+				keyPath = keyPath || [],
+				key;
 
-	values: function() {
-		return [].concat(this._values);
-	},
-
-	indexOf: function(key) {
-		var i = this._keys.indexOf(key);
-		return i === -1 ? this._keys.length : i;
-	},
-
-	getIn: function(keyPath) {
-		var current = this,
-			keyPath = keyPath || [],
-			key;
-
-		for(var i= 0, len=keyPath.length; i<len; i++) {
-			key = keyPath[i];
-			current = current.get(key);
-			if (current === undefined) {
-				break;
-			}
-		}
-
-		return current;
-	},
-
-	setIn: function(keyPath, value) {
-		var current = this,
-			keyPath = keyPath || [],
-			_cur;
-
-		keyPath.forEach(function(key, index) {
-			if (index + 1 === keyPath.length) {
-				current.set(key, value);
-			} else {
-				_cur = current.get(key);
-				if (!_cur) {
-					_cur = Map();
-					current.set(key, _cur);
+			for(var i= 0, len=keyPath.length; i<len; i++) {
+				key = keyPath[i];
+				current = current.get(key);
+				if (current === undefined) {
+					break;
 				}
-				current = _cur;
 			}
-		});
 
-		return this;
-	},
+			return current;
+		},
 
-	each: function(fn) {
-		var self = this;
-		this._keys.forEach(function(key) {
-			return fn(key, self._values[self.indexOf(key)]);
-		});
+		setIn: function(keyPath, value) {
+			var current = this,
+				keyPath = keyPath || [],
+				_cur;
 
-		return this;
+			keyPath.forEach(function(key, index) {
+				if (index + 1 === keyPath.length) {
+					current.set(key, value);
+				} else {
+					_cur = current.get(key);
+					if (!_cur) {
+						_cur = map();
+						current.set(key, _cur);
+					}
+					current = _cur;
+				}
+			});
+
+			return this;
+		},
+
+		indexOf: function(key) {
+			var i = this.keys.indexOf(key);
+			return i === -1 ? this.keys.length : i;
+		},
+
+		keyOf: function(val) {
+			return this.keys[this.values.indexOf(val)];
+		},
+
+		each: function(fn) {
+			var self = this;
+			this.keys.forEach(function(key) {
+				return fn(key, self.values[self.indexOf(key)]);
+			});
+
+			return this;
+		},
+
+		remove: function(key) {
+			var i = this.indexOf(key);
+			if (i !== this.keys.length) {
+				this.keys.splice(i, 1);
+				this.values.splice(i, 1);
+			}
+			return this;
+		},
+
+		clear: function(keys, values) {
+			this.keys = keys || [];
+			this.values = values || [];
+			return this;
+		},
+
+		noConflict: function() {
+			root.map = previous_map;
+			return map;
+		}
+	};
+
+	if (typeof exports !== 'undefined') {
+		if (typeof module !== 'undefined' && module.exports) {
+			exports = module.exports = map;
+		}
+		exports.map = map;
+	} else {
+		root.map = map;
 	}
-};
+
+}).call(this);
+
